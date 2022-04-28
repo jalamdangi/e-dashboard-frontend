454cebb441270fd11149df1ae265a65ae8136531
Jalam Dangi <JalamD@bpl.netlink.com>
2022-04-28 18:33:50 +0530

    evening frontend changes

--- src/Components/AddProduct.js
+++ src/Components/AddProduct.js
@@ -1,9 +1,90 @@
-import React from "react";
-const AddProduct = () =>{
-    return(
-        <div>
-            <h2>This is add product page</h2>
-        </div>
+import React, { useState } from "react";
+import addproduct_image from '../images/addproduct_image.svg'
+const AddProduct = () => {
+    const [productname, setProductname] = useState("")
+    const [productprice, setProductprice] = useState("")
+    const [productcategory, setProductcategory] = useState("")
+    const [productbrand, setProductbrand] = useState("")
+    const addProduct = async (e) => {
+        e.preventDefault()
+        // console.log(productname, productprice, productcategory, productbrand)
+        const userid = JSON.parse(localStorage.getItem('user'))._id;
+        // console.log(userid);
+        if (!productname || !productprice || !productcategory || !productbrand) {
+            alert("all field are required");
+        }else{
+            let result = await fetch('http://localhost:5000/addproduct', {
+                method: 'post',
+                body: JSON.stringify({ productname, productprice, productcategory, productbrand, userid }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            result = await result.json()
+            console.log(result);
+            setProductname("")
+            setProductprice("")
+            setProductcategory("")
+            setProductbrand("")
+        }
+    }
+    return (
+        <>
+            <div className="container bg-light border">
+                <div className="row">
+                    <div className="col-md-5 pt-5">
+                        <h4 className="addproduct-head">Add Your Product Here</h4>
+                        <form>
+                            <div className="form-group">
+                                <label>Product Name</label>
+                                <input
+                                    type="text"
+                                    onChange={(e) => setProductname(e.target.value)}
+                                    value={productname}
+                                    className="form-control"
+                                />
+                                <small className="form-text text-muted">
+                                    We'll never share your email with anyone else.
+                                </small>
+                            </div>
+                            <div className="form-group">
+                                <label>Product Price</label>
+                                <input
+                                    type="text"
+                                    onChange={(e) => setProductprice(e.target.value)}
+                                    value={productprice}
+                                    className="form-control"
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Product Category</label>
+                                <input
+                                    type="text"
+                                    onChange={(e) => setProductcategory(e.target.value)}
+                                    value={productcategory}
+                                    className="form-control"
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Company/Brand</label>
+                                <input
+                                    type="text"
+                                    onChange={(e) => setProductbrand(e.target.value)}
+                                    value={productbrand}
+                                    className="form-control"
+                                />
+                            </div>
+                            <button onClick={addProduct} type="submit" className="btn btn-success">
+                                Add Product
+                            </button>
+                        </form>
+                    </div>
+                    <div className="col-md-7">
+                        <img src={addproduct_image} className="addproduct-image" alt="error" />
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Product = () => {
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    getProduts();
+  }, [])
+  const getProduts = async () => {
+    let result = await fetch('http://localhost:5000/productlist');
+    result = await result.json()
+    setProducts(result)
+  }
+  // console.log(products);
+
   return (
-    <div>
-        <h2 className='text-center'>E - Dashboard</h2>
-    </div>
+    <>
+      <div className="container-fluid bg-light">
+        <h2 className='text-center p-2'>E - Dashboard</h2>
+        <div className="row">
+          {
+            products.map((item, index) => {
+              return (
+                <div className="col-sm-4 p-2" key={index.toString()}>
+                  <div className="card bg-info">
+                    <div className="card-body">
+                      <h5 className="text-capitalize">{index + 1} {item.productname}</h5>
+                      <p className="text-capitalize pl-2">Brand : {item.productbrand}</p>
+                      <p className="pl-2">Price : {item.productprice}</p>
+                      <div className="row">
+                        <div className="col-md-6"><a href="#" className="btn btn-warning">Chek More</a></div>
+                        <div className="col-md-6 text-right mt-2 text-capitalize">Category : {item.productcategory}</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )
+            })
+          }
+
+        </div>
+      </div>
+    </>
   )
 }
 
